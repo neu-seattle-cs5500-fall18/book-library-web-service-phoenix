5a74f2dbb7173ffa9b01287eb883a3531d02a7e4
xianyin <chen.xiany@husky.neu.edu>
2018-09-28 17:09:05 -0700

    'heroku'

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def start_service():
     return "Welcome to BookVector", 200
 
+if __name__ == '__main__':
+    app.run(debug=True)
