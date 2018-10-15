a5b68895ef611a10b82675804ba7f9fed1d6702f
xianyin <chen.xiany@husky.neu.edu>
2018-10-15 14:29:16 -0700

    reformat .json

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def start_service():
     return "Welcome to BookVector", 200
 
 
+@app.route('/swagger')
+def swagger_api():
+    return
+
+
+@app.route('/support')
+def support():
+    return "Please contact yu.jiah@husky.neu.edu; chen.xiany@husky.neu.edu", 200
+
+
 if __name__ == '__main__':
     app.run(debug=True)
