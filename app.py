036c08a95a96964d56c23e7a199df3f21ae3f58f
xianyin <chen.xiany@husky.neu.edu>
2018-12-04 18:27:21 -0800

    deploy to heroku

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ book_vector.init_app(app)
 api = Namespace('')
 book_vector.add_namespace(api)
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wgoclgfndpiece:175edbd766319631ba5a2c41bd3b1f428cfcdbab789263c1e882147c9a53ce0a@ec2-23-21-192-179.compute-1.amazonaws.com:5432/d138rvo7lslh7v'
 app.config['SECRET_KEY'] = 'secret_key'
 
