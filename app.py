45a59a775832a3b61893a39b8d1f47d8d1e7b731
xianyin <chen.xiany@husky.neu.edu>
2018-12-09 19:10:54 -0800

    'update'

--- app.py
+++ app.py
@@ -12,9 +12,11 @@ db.init_app(app)
 login_manager.init_app(app)
 book_vector.init_app(app)
 
+
 @app.before_first_request
 def create_tables():
     db.create_all()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
